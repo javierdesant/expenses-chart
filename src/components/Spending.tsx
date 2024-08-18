c03bffe688da48ed9d16c70d2a720a5cdd1b5cb8
JavierDeSant <j.desantiagosoto@gmail.com>
2024-08-18 03:43:02 +0200

    Add initial static chart

--- src/components/Spending.tsx
+++ src/components/Spending.tsx
@@ -6,20 +6,37 @@ interface SpendingProps {
  
 const Spending: FunctionComponent<SpendingProps> = () => {
     return (
-        <div className="flex flex-col max-w-[800px] w-full h-[420px] p-4 box-border bg-neutral-very-pale-orange rounded-xl">
-            <div className="w-11/12 self-center">
+        <div className="flex flex-col max-w-[800px] w-full h-[420px] py-8 box-border bg-neutral-very-pale-orange rounded-xl">
+            <div className="w-11/12 h-full self-center">
                 <h1 className="text-xl font-dm-sans font-bold text-neutral-dark-brown">Spending - Last 7 days</h1>
-                <p className="font-dm-sans text-xs">
-                    mon
-                    tue
-                    wed
-                    thu
-                    fri
-                    sat
-                    sun 
-                </p>
+                
+                <div className="flex flex-col h-full">
+
+                    <div className="h-36 w-full flex items-end space-x-2 my-4">
+                        <div className="flex-1 h-full border border-solid border-black bg-red-500"></div>
+                        <div className="flex-1 h-full border border-solid border-black bg-blue-500"></div>
+                        <div className="flex-1 h-full border border-solid border-black bg-green-500"></div>
+                        <div className="flex-1 h-full border border-solid border-black bg-yellow-500"></div>
+                        <div className="flex-1 h-full border border-solid border-black bg-purple-500"></div>
+                        <div className="flex-1 h-full border border-solid border-black bg-pink-500"></div>
+                        <div className="flex-1 h-full border border-solid border-black bg-indigo-500"></div>
+                    </div>
+                    
+                    <p className="font-dm-sans text-xs">
+                        mon
+                        tue
+                        wed
+                        thu
+                        fri
+                        sat
+                        sun 
+                    </p>
+                </div>
+
             </div>
+
             <hr className="my-4 w-11/12 border-neutral-light-brown self-center" />
+            
             <div className="flex flex-col font-dm-sans w-11/12 self-center">
                 <p className="text-neutral-medium-brown mb-2">
                     Total this month
